089b8fe175df4cd27b9f9b2ce022061685cd8d27
Juliette <juliette@ponsonnet.org>
2024-07-25 23:02:44 +0200

    ✨ Cap clair
    il faut un cap clair

--- app/Main.hs
+++ app/Main.hs
@@ -7,8 +7,11 @@ module Main where
 
 import Configuration.Dotenv
 import Control.Monad (void, when)
+import Control.Monad.IO.Class (liftIO)
+import Data.Char
 import Data.Functor ((<&>))
 import Data.Maybe (fromJust)
+import Data.Text (Text)
 import Data.Text qualified as Text
 import Data.Text.IO qualified as TIO
 import Discord
@@ -33,10 +36,13 @@ main = do
 
 eventHandler :: Event -> DiscordHandler ()
 eventHandler = \case
-  MessageCreate m -> when (isPing m && not (fromBot m)) do
-    void $ restCall (R.CreateReaction (messageChannelId m, messageId m) "eyes")
-    threadDelay (2 * 10 ^ 6)
-    void $ restCall (R.CreateMessage (messageChannelId m) "Pong!")
+  MessageCreate m -> do
+    when (isPing m && not (fromBot m)) do
+      void $ restCall (R.CreateReaction (messageChannelId m, messageId m) "eyes")
+      threadDelay (2 * 10 ^ 6)
+      void $ restCall (R.CreateMessage (messageChannelId m) "Pong!")
+    when (isCapClair m && not (fromBot m)) do
+      void $ restCall (R.CreateMessage (messageChannelId m) glucksmannLink)
   _ -> pure ()
 
 fromBot :: Message -> Bool
@@ -44,3 +50,13 @@ fromBot = userIsBot . messageAuthor
 
 isPing :: Message -> Bool
 isPing = ("ping" `Text.isPrefixOf`) . Text.toLower . messageContent
+
+glucksmannLink :: Text
+glucksmannLink = "https://cdn.discordapp.com/attachments/1264010718563270708/1264680541521838131/An_uAjxiF2sxvurM-hiLui2FhOYsJjVTd7yEwDZuOY2JFj9rtHHK1rX0_YIDAM5hQXNdvapmfRGHLmMACp6uJZmj.mp4?ex=66a40709&is=66a2b589&hm=d36cea0d7ba5fb612c65a22050b2914e929006d26fea30b00850628dd1b68303&"
+
+isCapClair :: Message -> Bool
+isCapClair =
+  ("capclair" `Text.isSuffixOf`)
+    . Text.filter isLetter
+    . Text.toLower
+    . messageContent
